abfb4c2740b69446ae1f3cc245acb1b0e9ab66a6
Elias Cecetka <elias_2000@web.de>
2022-10-18 20:55:35 +0200

    Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.scss";
+import { initializeApp } from "firebase/app";
+
 import Book from "./components/book/Book";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDbspHMfyqsnbFRPJVkzdsVBW8XSmyZAXU",
+  authDomain: "endless-story-beedf.firebaseapp.com",
+  projectId: "endless-story-beedf",
+  storageBucket: "endless-story-beedf.appspot.com",
+  messagingSenderId: "1076141221277",
+  appId: "1:1076141221277:web:72c35167cb6f45d041fb74",
+};
+
 function App() {
+  initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <Book />
