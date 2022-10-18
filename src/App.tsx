93f8f7fe730874a1c6c8e463f6ee1f7643688d50
Elias Cecetka <elias_2000@web.de>
2022-10-18 20:42:21 +0200

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import Book from "./components/book/Book";
 
 function App() {
   return (
     <div className="App">
-      <h1>Beans</h1>
+      <Book />
     </div>
   );
 }
